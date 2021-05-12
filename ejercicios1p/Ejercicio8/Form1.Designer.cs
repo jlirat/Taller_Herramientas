92bbc33033ecd7bbf9e9fffca76083fffa69e917
Jose Lira <jl.lira@gmail.com>
2021-05-11 22:57:16 -0500

    Ejercicio 8 - Parcial 1

---
+++ ejercicios1p/Ejercicio8/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Ejercicio8
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.areaTrabajo = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.areaTrabajo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // areaTrabajo
+            // 
+            this.areaTrabajo.Location = new System.Drawing.Point(6, 12);
+            this.areaTrabajo.Name = "areaTrabajo";
+            this.areaTrabajo.Size = new System.Drawing.Size(1577, 304);
+            this.areaTrabajo.TabIndex = 0;
+            this.areaTrabajo.TabStop = false;
+            this.areaTrabajo.Paint += new System.Windows.Forms.PaintEventHandler(this.areaTrabajo_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1595, 328);
+            this.Controls.Add(this.areaTrabajo);
+            this.Name = "frmPrincipal";
+            this.Text = "Juego de la Tortuga";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.frmPrincipal_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.frmPrincipal_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.areaTrabajo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox areaTrabajo;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ejercicios1p/Ejercicio8/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio8
+{
+    public partial class frmPrincipal : Form
+    {
+        Image tortuga = null;
+        int x, y;
+        int fx, fy;
+        int mono = 0;
+        double velocidad = .01;
+        int tiempo = 200;
+        bool tipo = false;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            mono++;
+            if (mono >= 3) mono = 0;
+            if (tipo) fy = 250;
+            else
+                fy = 0;
+            Console.WriteLine("Mono:" + mono);
+            CollisionCheck();
+            areaTrabajo.Invalidate();
+            
+        }
+
+        private void CollisionCheck()
+        {
+            // Si llega al límite derecho
+            if ((x + 250) >= areaTrabajo.Width)
+            {
+                x = areaTrabajo.Width - 250;
+            }
+            if (x <= 0)
+            {
+                x = 0;
+            }
+        }
+
+        private void frmPrincipal_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Console.WriteLine(e.KeyChar.ToString());
+            int distancia = (int)(velocidad * tiempo);
+            if (e.KeyChar == 65)
+            {
+                x-=distancia;
+                tipo = true;
+            }
+            if(e.KeyChar == 68)
+            {
+                x+=distancia;
+                tipo = true;
+            }
+                
+            
+        }
+
+        private void frmPrincipal_KeyUp(object sender, KeyEventArgs e)
+        {
+            Console.WriteLine("Liberando");
+            tipo = false;
+        }
+
+        public frmPrincipal()
+        {
+            InitializeComponent();
+            tortuga = Image.FromFile("Recursos\\tortuga_array.png");
+            x = y = 0;
+            fx = fy = 0;
+            tipo = false;
+            timer1.Enabled = true;
+            timer1.Start();
+        }
+        private void areaTrabajo_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawImage(tortuga, 
+                            new Rectangle(x,y,250,250),
+                            fx+mono*250,fy,250,255, GraphicsUnit.Pixel);
+        }
+    }
+}
