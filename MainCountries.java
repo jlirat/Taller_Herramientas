a3d17d3c46a7201c8c36583cfad3834213460aac
Jose Lira <jl.lira@gmail.com>
2021-05-11 19:54:44 -0500

    Ejercicio countries

---
+++ MainCountries.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+import ejercicios2p.json.CountryUI;
+
+public class MainCountries {
+    public static void main(String args[]) {
+        CountryUI w = new CountryUI();
+        w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
